dc1cfb9a3d424632394ff4d6b18e4a8b04057838
AlexLP <alexlepoupon@gmail.com>
2018-01-26 15:23:31 +0100

    Exercise 3

--- src/app.js
+++ src/app.js
@@ -3,4 +3,13 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/', function(req, res) {
+  res.send('You need to go to /bingo');
+});
+
+app.get('/bingo', function(req, res) {
+  res.send('Bingo');
+});
+  
+
 module.exports = app;
